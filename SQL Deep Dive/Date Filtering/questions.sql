8c6527a9e875049f2d524983c0a4e7d13e4d3659
Alona S <alona.s.kry@gmail.com>
2025-04-24 17:40:06 +0100

    Created Dates, Distinct and Sorting Queries

--- SQL Deep Dive/Date Filtering/questions.sql
+++ SQL Deep Dive/Date Filtering/questions.sql
@@ -4,7 +4,8 @@
 * Question: Get me all the employees above 60, use the appropriate date functions
 */
 
-SELECT * FROM employees;
+SELECT *, AGE(birth_date) AS "Age" FROM employees
+WHERE EXTRACT(YEAR FROM AGE(birth_date)) > 60;
 
 /*
 * DB: Employees
@@ -12,7 +13,8 @@ SELECT * FROM employees;
 * Question: How many employees where hired in February?
 */
 
-SELECT * FROM employees;
+SELECT COUNT(*) As "Amount of Employees" FROM employees
+WHERE date_part('month', hire_date) = 2;
 
 /*
 * DB: Employees
@@ -20,7 +22,9 @@ SELECT * FROM employees;
 * Question: How many employees were born in november?
 */
 
-SELECT * FROM employees;
+
+SELECT COUNT(*) As "Born in November" FROM employees
+WHERE date_part('month', birth_date) = 11;
 
 /*
 * DB: Employees
@@ -28,7 +32,7 @@ SELECT * FROM employees;
 * Question: Who is the oldest employee? (Use the analytical function MAX)
 */
 
-SELECT * FROM employees;
+SELECT MAX(AGE(birth_date)) AS "Age" FROM employees;
 
 /*
 * DB: Store
@@ -36,5 +40,6 @@ SELECT * FROM employees;
 * Question: How many orders were made in January 2004?
 */
 
-SELECT * FROM orders;
+SELECT COUNT(*) AS "Orders Amount" FROM orders
+WHERE EXTRACT(MONTH FROM orderdate)=1 AND EXTRACT(YEAR FROM orderdate)=2004;
 
