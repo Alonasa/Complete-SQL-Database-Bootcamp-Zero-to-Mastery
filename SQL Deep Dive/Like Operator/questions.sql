018560751465a7d8897138f32735333fd9946334
Alona S <alona.s.kry@gmail.com>
2025-04-24 15:11:28 +0100

    Finished IN and LIKE queries

--- SQL Deep Dive/Like Operator/questions.sql
+++ SQL Deep Dive/Like Operator/questions.sql
@@ -7,13 +7,17 @@
 */
 SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
 
-
+SELECT first_name, last_name, EXTRACT(YEAR FROM AGE(birth_date)) AS Age FROM employees
+WHERE first_name LIKE 'M%'
 /*
 * DB: Employees
 * Table: employees
 * Question: How many people's name start with A and end with R?
 * Expected output: 1846
 */
+
+SELECT COUNT(*) AS "Names like A...r" FROM employees
+WHERE first_name LIKE 'A%r';
 
                                                   
 /*
@@ -23,7 +27,8 @@ SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
 * Expected output: 4211 
 */
 
-
+SELECT COUNT(*) AS "Zips with 2 in it amount" FROM customers
+WHERE CAST(zip AS text) LIKE '%2%';
 
 /*
 * DB: Store
@@ -31,6 +36,9 @@ SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
 * Question: How many people's zipcode start with 2 with the 3rd character being a 1.
 * Expected output: 109 
 */
+
+SELECT COUNT(*) AS "Zips with 2 and 3rd 1 amount" FROM customers
+WHERE CAST(zip AS text) LIKE '2_1%';
 
 
 /*
@@ -40,4 +48,7 @@ SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
 * Replace null values with "No State"                                                  
 * Expected output: https://imgur.com/AVe6G4c
 */
+
+SELECT COALESCE(state, 'No state') AS "States", country FROM customers
+WHERE CAST(phone AS text) LIKE '302%';
 
