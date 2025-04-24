496ba2f8eba7c19209fedd85899aa0b46b50abc8
Alona S <alona.s.kry@gmail.com>
2025-04-24 13:39:03 +0100

    added answers for BETWEEN

--- SQL Deep Dive/BETWEEN + AND/questions.sql
+++ SQL Deep Dive/BETWEEN + AND/questions.sql
@@ -4,8 +4,13 @@
 /*
 * Write your query here
 */
+SELECT * FROM customers
+WHERE income < 50000 AND age BETWEEN 30 AND 50;
 
 -- What is the average income between the ages of 20 and 50? (Including 20 and 50)
 /*
 * Write your query here
 */
+
+SELECT AVG(income) AS "Average income 20-50 y.o" FROM customers
+WHERE age BETWEEN 20 AND 50;
